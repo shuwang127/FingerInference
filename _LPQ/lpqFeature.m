135b0eee03477787cdf9010e6426b80bcb8cee2d
Shu W <shuwang127@yahoo.com>
2019-11-26 20:04:36 -0500

    Add files via upload

--- _LPQ/lpqFeature.m
+++ _LPQ/lpqFeature.m
@@ -5,6 +5,9 @@ function [ lpq_feat ] = lpqFeature( data )
 lpq_feat = [];
 for i = 1 : length(data)
     img = data{i};
+    if size(img, 3) > 1
+        img = rgb2gray(img);
+    end
     feat = lpq(img, 5);
     lpq_feat = [lpq_feat; feat];
 end
